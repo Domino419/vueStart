8e19c7085ddaaae79f00200ab64d7901f294b2aa
Domino419 <noris419@naver.com>
2025-01-18 00:49:20 +0900

    ch03_myapp1 str

---
+++ ch03/ch03_myapp1/postcss.config.js
@@ -0,0 +1,15 @@
+
+/**
+ * module        : postcss.config.js
+ * date          : 25-01-18
+ * description   : PostCSS 설정 파일로, Tailwind CSS와 Autoprefixer 플러그인을 설정합니다.
+ */
+module.exports = {
+    plugins: {
+        // Tailwind CSS 플러그인 설정
+        tailwindcss: {},
+
+        // Autoprefixer 플러그인 설정
+        autoprefixer: {},
+    },
+}
--- ch03/ch03_myapp1/src/main.js
+++ ch03/ch03_myapp1/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+/**
+ * method        : main
+ * date          : 2025-01-18
+ * return        : void - Vue 앱을 초기화하고, 지정된 DOM 요소에 마운트한다.
+ */
+import { createApp } from 'vue';  // Vue 라이브러리에서 createApp 함수를 임포트
+import App from './App.vue';       // 루트 컴포넌트 App.vue 임포트
+import './assets/tailwind.css';    // Tailwind CSS 스타일시트 임포트
 
-createApp(App).mount('#app')
+const app = createApp(App);      // Vue 앱 인스턴스를 생성하고 루트 컴포넌트 App을 사용
+app.mount('#app');                // id가 'app'인 DOM 요소에 Vue 앱을 마운트
