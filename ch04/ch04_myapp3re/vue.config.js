6466d85a0cfd37065011b4ca089ce1363c144543
Domino419 <noris419@naver.com>
2025-01-23 02:19:59 +0900

    vue.config.js 수정

--- ch04/ch04_myapp3re/vue.config.js
+++ ch04/ch04_myapp3re/vue.config.js
@@ -17,3 +17,10 @@ module.exports = defineConfig({
     });
   },
 });
+
+
+// 교재와 다름.
+// 크롬 개발자 도구에서 에러 메시지 확인
+// main.js:5 Feature flag __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ is not explicitly defined. You are running the esm-bundler build of Vue, which expects these compile-time feature flags to be globally injected via the bundler config in order to get better tree-shaking in the production bundle. For more details, see https://link.vuejs.org/feature-flags.
+// __VUE_PROD_HYDRATION_MISMATCH_DETAILS__ 컴파일 타임 플래그가 정의되지 않아서 뜨는 에러로 해당 플래그를 vue.config.js에 추가하여 에러 메시지 클리어.
+
