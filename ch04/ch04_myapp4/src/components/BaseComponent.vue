6eff46bb3c33373b71a23a9e494ce3490ceda0d7
Domino419 <noris419@naver.com>
2025-01-23 23:21:37 +0900

    컴포넌트 심화 실습 ing

---
+++ ch04/ch04_myapp4/src/components/BaseComponent.vue
@@ -0,0 +1,10 @@
+<template>
+<div :class="$style.container">
+  <h1> Vue 3 app with PureCss </h1>
+  <slot></slot>  <!--기본 슬록 -->
+</div>
+</template>
+
+<style module>
+.container {padding: 20px ; background-color: #f4f4f4 ;}
+</style>
---
+++ ch04/ch04_myapp4/src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue' ;
+import App from './App.vue' ;
+import 'purecss/build/base.css' ;  // pureCSS 파일 임포트
+
+createApp(App).mount('#app')
